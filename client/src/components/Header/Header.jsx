bd6a171ebdf94e05b2b9eb977ed9f0c6fa7f432d
j-cort <81960011+j-cort@users.noreply.github.com>
2022-02-13 14:10:10 +0000

    add placeholders for sign in

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -10,14 +10,36 @@ const Header = ({setSession}) => {
   }
 
   return (
-    <div className="nav-header ui secondary pointing menu">
-      {/* <Link to='/items' className='item'>
-        ITEMS
-      </Link> */}
-      <div className="logo item">
-        <p className="blue" href="#" onClick={signOut}>Sign Out</p>
+    <div className="ui borderless top massive menu">
+
+      <Link to='/items' className="item">
+        Local Lab
+      </Link>
+
+      <div className='right menu'>
+
+        <div className="item">
+          <div class="ui left green icon label">
+            <i class="dove icon"></i>
+            8
+          </div>
+          <div class="ui left blue icon label">
+            <i class="gift icon"></i>
+            6
+          </div>
+        </div>
+  
+        <Link to='/profile' className="item">
+          <img src="https://fomantic-ui.com/images/avatar/small/elliot.jpg" className="ui mini circular image" alt=""/>
+        </Link>
+
+       
+      
+        <div className="item" onClick={signOut}>
+          Sign Out
+        </div>
       </div>
-     
+    
     </div>
   )
 } 
