7b30cb761dbea9c8c37ee2e13e9e43e874efd239
Muhammad Choudhry <muhammadshahzaibc@gmail.com>
2022-02-13 15:05:44 +0000

    changed port and added script

--- server/index.js
+++ server/index.js
@@ -81,8 +81,9 @@ app.get("/", (req, res) => {
 });
 
 
+const port = process.env.PORT || 3000
 // OPEN EXPRESS ON PORT 4000
-app.listen(4000, () => {
-  console.log("Listening on port 4000");
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
 
