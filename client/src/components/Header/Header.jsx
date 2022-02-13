bd9fbe5f0b6742622d278a9145179db7bc71c2bd
j-cort <81960011+j-cort@users.noreply.github.com>
2022-02-13 14:16:03 +0000

    add placeholdrs to header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -18,26 +18,26 @@ const Header = ({setSession}) => {
 
       <div className='right menu'>
 
-        <div className="item">
+        {/* <div className="item">
           <div class="ui left green icon label">
             <i class="dove icon"></i>
             8
           </div>
+
           <div class="ui left blue icon label">
             <i class="gift icon"></i>
             6
           </div>
-        </div>
+        </div> */}
   
         <Link to='/profile' className="item">
           <img src="https://fomantic-ui.com/images/avatar/small/elliot.jpg" className="ui mini circular image" alt=""/>
         </Link>
 
-       
-      
-        <div className="item" onClick={signOut}>
+        <Link to='/' className="item">
           Sign Out
-        </div>
+        </Link>
+      
       </div>
     
     </div>
