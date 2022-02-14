d3d1f9c07a1613d91eb0d9809fbe257e7b9d179f
Sabrina <sabrinalord@msn.com>
2022-02-14 15:01:12 +0000

    added css styling to user photo to prevent stretched images

--- client/src/components/Users/UserProfile.jsx
+++ client/src/components/Users/UserProfile.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import UsersItemList from '../Items/UsersItemsList';
 import avatar from "../images/user-avatar.jpeg";
+import testImage from "../images/linda.jpeg";
+
+
 
 
 const UserProfile = (session) => {
@@ -14,9 +17,11 @@ return (
      <div className="profile-wrapper">
         <div className="user-info-container">
               <div className="avatar-container">
-                <img alt="user profile" className="avatar" src={avatar}></img>
+                <img alt="user profile" className="avatar" src={testImage}></img>
               </div>
                 <span className="profile-username">{userDetails.user.username}</span>
+                <span className="profile-location">{userDetails.user.location}</span>
+
           </div>
 
         <div className = "profile-items-container">
