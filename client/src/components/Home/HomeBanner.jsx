06de7ea00a37947cc822c38d6271114bca9d3c70
Sabrina <sabrinalord@msn.com>
2022-02-15 12:49:13 +0000

    added media queries

--- client/src/components/Home/HomeBanner.jsx
+++ client/src/components/Home/HomeBanner.jsx
@@ -7,9 +7,10 @@ import bannerImage from "../../components/images/woman-with-drill.png";
 const HomeBanner = () => {
 
   return( 
-      <div class="banner-wrapper">
+      <div className="banner-wrapper">
+        <div className="mobile-hero"></div>
         <img src={bannerImage} alt="man with a drill"></img> 
-          <div class="overlay">
+          <div className="overlay">
           <span className="site-header">No drill?</span>
           <span className="site-header">No problem.</span>
           <span className="site-tagline">Local lending and borrowing made easy.</span>
