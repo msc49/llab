c3732be7974c7a5a8e63bfc8376012a925946cf7
j-cort <81960011+j-cort@users.noreply.github.com>
2022-02-16 17:45:23 +0000

    requests added to request page

--- client/src/components/Requests/RequestItem.jsx
+++ client/src/components/Requests/RequestItem.jsx
@@ -6,14 +6,45 @@ import '../../img/image.png'
 
  const RequestItem = ({requestItem, session}) => {
 
-  const { _id: itemId, name, description, images, available, requests } = requestItem
-    console.log(requestItem)
-    console.log('REQUESTS', requests)
+  const { _id: itemId, name, description, images, requests } = requestItem
     // set rating when we get value from item
     let r = Math.floor(Math.random() * 5) + 1;
     const RatingStar = () => (
       <Rating icon='star' defaultRating={r} maxRating={5} disabled/>
     )
+    
+    
+    const reqs = requests.map(request => request.requester)
+    const singleReq = reqs.map(singleReq => {
+      return (
+        <div className='ui cards'>
+          
+          <div class="card">
+            <div class="content">
+              <img class="right floated mini ui image" src="/images/avatar/large/elliot.jpg" alt=''/>
+              <div class="header">
+              {singleReq.name}
+              </div>
+              <div class="meta">
+                {singleReq.location}
+                {singleReq.email}
+              </div>
+              <div class="description">
+                {singleReq.name} requested to borrow your {name}
+              </div>
+            </div>
+            <div class="extra content">
+              <div class="ui two buttons">
+                <div class="ui basic green button">Approve</div>
+                <div class="ui basic red button">Decline</div>
+              </div>
+            </div>
+          </div>
+        
+        </div>
+      )
+    } )
+
 
   return (
     // <div>
@@ -41,7 +72,14 @@ import '../../img/image.png'
         <br/>
         <p></p>
         <div className="meta">
-          Currently Request By: {requests[0].requester.name}
+
+        {/* {requests ? requests.forEach(request => (request.requester)) : ""}
+        {requests.forEach(request => (request.requester))} */}
+        
+        {singleReq}
+   
+          
+      
           {/* {borrower ? <p><span className="ui blue text">Availble Soon</span></p>: <p><span className="ui green text">Available now</span></p>} */}
         </div>
       </div>
