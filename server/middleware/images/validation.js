305bf28d76c96ecb8fd7e9f6c360274305d438ba
Muhammad Choudhry <muhammadshahzaibc@gmail.com>
2022-04-30 22:08:21 +0100

    removed unused code from validation.js

--- server/middleware/images/validation.js
+++ server/middleware/images/validation.js
@@ -1,37 +1,5 @@
 module.exports = (req, res, next) => {
 
-  // if (typeof req.file === "undefined" || typeof req.body === "undefined") {
-  //   return res.status(400).json({
-  //     errors: "Problem with sending data",
-  //   });
-  // }
-
-
-// let { path } = req.file
-// console.log('PATH', path)
-
-// if (
-//   !req.file.mimetype.includes("jpeg") &&
-//   !req.file.mimetype.includes("jpg") &&
-//   !req.file.mimetype.includes("png")
-// ) { 
-//   return res.status(400).json({
-//     errors: "file not supported"
-//   })
-// }
-
-// if(req.file.size > 1024 * 1024) {
-//   return res.status(400).json({
-//     errors: "File is Too large"
-//   })
-// }
-
-// if(!path) {
-//   return res.status(400).json({
-//     errors: "file path is required"
-//   })
-// }
-
 next()
 
 };
